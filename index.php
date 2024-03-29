de156726ab26927e4dda19b719d1b465291a0f7f
shreyakarni <165394495+shreyakarni@users.noreply.github.com>
2024-03-29 19:11:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>index</title>
+<style>
+.butto{
+ padding: 10px;
+ font-size: 20px;
+ border-radius: 5px;
+ }
+</style>
+</head>
+<body style="background-image:url(img2.jpg);">
+<center>
+<h1><i><u>***Apartment Management System***</i></u></h1>
+<h1><marquee>Developed by Surekha G Y</marquee></h1>
+<img src ="apartment.jpg" border=5px solid #510;></img>
+
+
+<h3>APARTMENT MANAGEMENT SYSTEM is a complete solution in web based application. One of the major tasks of managing apartment is ensuring smooth transactions with large number of tenants/owners that occupy these apartments. APMS is a 'Housing Society Accounting Software' with complete solution for rental bill generation, tracking expenses and income, tracking upkeep and repair, staff administration, complaint management, housekeeping service, baby sitting services, parking, visitors information handling, forum facility, selling and buying of apartments, reporting services, online payment service etc.</h3>
+<h2>Choose your module</h2>
+<p ><a href="user_module/user_login.php"><input type="button" class="butto" value="User" size= "100"></a></p>
+<p><a href="admin_module/admin_login.php"><input type="button" class="butto" value="Admin"></a></p>
+</center>
+</body>
+</html>
